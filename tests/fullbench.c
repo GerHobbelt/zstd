0638bc77f9eb936e9a0285d3f97b6374d8baa6b9
Ger Hobbelt <ger@hobbelt.com>
2025-07-27 21:31:00 +0200

    fix compiler errors due to us using libassert as a system assert replacement: this is one scenario where assert() macros are NOT expressions -- unless we explicitly instruct libassert to generate *assertion expressions*!

--- tests/fullbench.c
+++ tests/fullbench.c
@@ -15,6 +15,7 @@
 #ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS /* disable Visual warning that it doesn't like fopen() */
 #endif
+#define LIBASSERT_ASSERT_IS_EXPRESSION
 #define ZSTD_DISABLE_DEPRECATE_WARNINGS /* No deprecation warnings, we still bench some deprecated functions */
 #include <limits.h>
 #include "util.h"        /* Compiler options, UTIL_GetFileSize */
